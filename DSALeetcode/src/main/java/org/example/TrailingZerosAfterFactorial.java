955ea23ec33faae78957c390428ef034a7f7eb2d
Karthik Vanabhojana <vanabhojana.k@northeastern.edu>
2024-01-10 16:06:05 -0500

    Commit the jumpgame leetcode q55

---
+++ DSALeetcode/src/main/java/org/example/TrailingZerosAfterFactorial.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class TrailingZerosAfterFactorial {
+public static void main(String[] args) {
+	 Scanner sc=new Scanner(System.in);
+	  
+	  System.out.println("Input value to count digit");
+	  int input=sc.nextInt();
+	  
+	  
+	  int res=trailingzeros(input);
+	  
+	  System.out.println(res);
+
+}
+
+private static int trailingzeros(int input) {
+	// TODO Auto-generated method stub
+	
+	int res=0;
+	for(int i=5; i<=input;i=i*5) {
+		res=res+input/i;
+		
+	}
+	return res;
+}
+
+}
