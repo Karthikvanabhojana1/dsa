9905030c2987601b9e2525d8043da98a1d13126b
Karthik Vanabhojana <vanabhojana.k@northeastern.edu>
2024-04-28 13:25:10 -0400

    Commiting the codes on basic Java when revising Basics of Java

---
+++ DSALeetcode/src/main/java/org/example/java/basics/day1/LogicalSwitchNew.java
@@ -0,0 +1,19 @@
+package org.example.java.basics.day1;
+
+public class LogicalSwitchNew {
+    public static void main(String[] args) {
+         String day ="Monday";
+
+         // after java 12 we can use switch as expression and no break; if not using colon you can use yield: "Wakeup on 6:00AM";"
+         String result=switch (day){
+             case "Saturday","Sunday"->
+                 "Wakeup on 6:00AM";
+             case "Monday"->
+                "Wakeup on 8:00AM";
+             default->
+                "Wakeup on 7:00AM";
+
+         };
+        System.out.println(result);
+    }
+}
---
+++ DSALeetcode/src/main/java/org/example/java/basics/day1/TypeCasting.java
@@ -0,0 +1,21 @@
+package org.example.java.basics.day1;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        byte b1=123;
+
+        System.out.println("b1 "+ b1);
+        int i=267;
+        b1=(byte) i;
+        System.out.println("after typecasting "+b1);
+
+//type casting if number of bytes of data base in right hand side is greater than left hand side
+        float f1=5.7f;
+        i=(int) f1;
+        System.out.println("i after type casting " +i);
+        int i2=b1;
+
+        //type conversion  if number of bytes of data base in left hand side is greater than right hand side
+        System.out.println("i2 after conversion "+i2);
+    }
+}
