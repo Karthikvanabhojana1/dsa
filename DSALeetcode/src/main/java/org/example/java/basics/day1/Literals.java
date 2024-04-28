cd7a0f84cf2712e7fede75229786a99c69c933df
Karthik Vanabhojana <vanabhojana.k@northeastern.edu>
2024-04-28 13:25:10 -0400

    Commiting the codes on basic Java when revising Basics of Java

---
+++ DSALeetcode/src/main/java/org/example/java/basics/day1/Literals.java
@@ -0,0 +1,20 @@
+package org.example.java.basics.day1;
+
+public class Literals {
+    public static void main(String[] args) {
+        int num1=5;
+        System.out.println(num1);
+        //binary
+        int num2=0b101;
+        System.out.println(num2);
+        int num3=0x7E;
+        System.out.println(num3);
+        int num4=10_00_00_00;
+        System.out.println(num4);
+        double num5=12e10;
+        System.out.println(num5);
+        char c='a';
+        c++;
+        System.out.println(c);
+    }
+}
