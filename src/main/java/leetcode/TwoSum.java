f77f1e96f6e5fdc9786637a040ef0598f53b1ca8
Karthik Vanabhojana <vanabhojana.k@northeastern.edu>
2024-09-24 06:58:19 -0500

    practicing leetcode between 14 to 24 september ArraysMedium

--- src/main/java/leetcode/TwoSum.java
+++ src/main/java/leetcode/TwoSum.java
@@ -1,7 +1,22 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer,Integer> map=new HashMap<>();
+        int length=nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            int rem=target-nums[i];
+            if (map.containsKey(nums[i])){
+                return new int[]{i,map.get(nums[i])};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+
 
     }
 }
